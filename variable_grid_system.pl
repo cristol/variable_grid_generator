0113266a7e05dc38e586d4bb7179c97a01ee106e
Stephen Cristol <stephen@bee.net>
2016-01-17 00:17:17 -0500

    Factor out common code; eliminate use of tab characters; added whitespace for readability; reduce number of blank lines

--- variable_grid_system.pl
+++ variable_grid_system.pl
@@ -30,15 +30,16 @@ my $margin_width_l = $margin_width;
 my $margin_width_r = $margin_width;
 $margin_width_l += 1
   if $uneven_gutter;
-my $overall_width = $num_cols * ( $col_width + $gutter_width );
+my $unit_width = $col_width + $gutter_width;
+my $overall_width = $num_cols * $unit_width;
 
 print <<"EOT";
 /*
-	Variable Grid System.
-	Learn more ~ http://www.spry-soft.com/grids/
-	Based on 960 Grid System - http://960.gs/
+  Variable Grid System.
+  Learn more ~ http://www.spry-soft.com/grids/
+  Based on 960 Grid System - http://960.gs/
 
-	Licensed under GPL and MIT.
+  Licensed under GPL and MIT.
 */
 
 /*
@@ -49,40 +50,37 @@ print <<"EOT";
   Note: IE6 works fine without this fix.
 */
 
-body {
-  min-width: ${overall_width}px;
-}
+body { min-width: ${overall_width}px; }
 
 /* Containers
 ----------------------------------------------------------------------------------------------------*/
 .container_$num_cols {
-	margin-left: auto;
-	margin-right: auto;
-	width: ${overall_width}px;
+  width:        ${overall_width}px;
+  margin-left:  auto;
+  margin-right: auto;
 }
 
 /* Grid >> Global
 ----------------------------------------------------------------------------------------------------*/
 
-
 EOT
 
-my $grid_str = join ",\n", map { ".grid_$_" } 1..$num_cols;
+my $grid_str = join ",\n", map { ".grid_$_" } 1 .. $num_cols;
 $grid_str .= <<"EOT";
  {
-\tdisplay:inline;
-\tfloat: left;
-\tposition: relative;
-\tmargin-left: ${margin_width_l}px;
-\tmargin-right: ${margin_width_r}px;
+  display:      inline;
+  float:        left;
+  position:     relative;
+  margin-left:  ${margin_width_l}px;
+  margin-right: ${margin_width_r}px;
 }
 EOT
 print $grid_str, "\n\n\n";
 
-my $push_pull_str = join ",\n", map { ".push_$_, .pull_$_" } 1..$num_cols;
+my $push_pull_str = join ",\n", map { ".push_$_, .pull_$_" } 1 .. $num_cols;
 $push_pull_str .= <<"EOT";
  {
-\tposition:relative;
+  position: relative;
 }
 EOT
 print $push_pull_str;
@@ -90,21 +88,14 @@ print $push_pull_str;
 
 print <<"EOT";
 
-
 /* Grid >> Children (Alpha ~ First, Omega ~ Last)
 ----------------------------------------------------------------------------------------------------*/
 
-.alpha {
-	margin-left: 0;
-}
-
-.omega {
-	margin-right: 0;
-}
+.alpha { margin-left:  0; }
+.omega { margin-right: 0; }
 
 /* Grid >> $num_cols Columns
 ----------------------------------------------------------------------------------------------------*/
-
 
 EOT
 
@@ -112,65 +103,35 @@ my $temp_width;   # Recyclable
 
 my $container_str = '';
 $temp_width = $col_width;
-for my $i ( 1..$num_cols ) {
-  $container_str .= ".container_$num_cols .grid_$i {\n"
-                  . "\twidth:${temp_width}px;\n"
-                  . "}\n\n";
-  $temp_width += $col_width + $gutter_width;
+for my $i ( 1 .. $num_cols ) {
+  $container_str .= ".container_$num_cols .grid_$i { width: ${temp_width}px; }\n";
+  $temp_width += $unit_width;
 }
 print $container_str;
 
 simple_spacer_header( 'Prefix Extra Space' );
 
-my $prefix_str = '';
-$temp_width = $col_width + $gutter_width;
-for my $i ( 1..$num_cols - 1 ) {
-  $prefix_str .= ".container_$num_cols .prefix_$i {\n"
-              . "\tpadding-left:${temp_width}px;\n"
-              . "}\n\n";
-  $temp_width += $col_width + $gutter_width;
-}
-print $prefix_str;
+$temp_width = 0;
+my $px_map = { map { $_ => ( $temp_width += $unit_width ) } 1 .. $num_cols - 1 };
+
+generate_classes( $num_cols - 1, $px_map, ".container_$num_cols .prefix_%-4d { padding-left:  %5dpx; }\n" );
 
 simple_spacer_header( 'Suffix Extra Space' );
 
-my $suffix_str = '';
-$temp_width = $col_width + $gutter_width;
-for my $i ( 1..$num_cols - 1 ) {
-  $suffix_str .= ".container_$num_cols .suffix_$i {\n"
-              . "\tpadding-right:${temp_width}px;\n"
-              . "}\n\n";
-  $temp_width += $col_width + $gutter_width;
-}
-print $suffix_str;
+generate_classes( $num_cols - 1, $px_map, ".container_$num_cols .suffix_%-4d { padding-right: %5dpx; }\n" );
 
 simple_spacer_header( 'Push Space' );
 
-my $push_str = '';
-$temp_width = $col_width + $gutter_width;
-for my $i ( 1..$num_cols - 1 ) {
-  $push_str .= ".container_$num_cols .push_$i {\n"
-            . "\tleft:${temp_width}px;\n"
-            . "}\n\n";
-  $temp_width += $col_width + $gutter_width;
-}
-print $push_str;
+generate_classes( $num_cols - 1, $px_map, ".container_$num_cols .push_%-4d   { left:          %5dpx; }\n" );
 
 simple_spacer_header( 'Pull Space' );
 
-my $pull_str = '';
-$temp_width = $col_width + $gutter_width;
-for my $i ( 1..$num_cols - 1 ) {
-  $pull_str .= ".container_$num_cols .pull_$i {\n"
-            . "\tleft:-${temp_width}px;\n"
-            . "}\n\n";
-  $temp_width += $col_width + $gutter_width;
-}
-print $pull_str;
+$temp_width = 0;
+$px_map = { map { $_ => ( $temp_width -= $unit_width ) } 1 .. $num_cols - 1 };
+
+generate_classes( $num_cols - 1, $px_map, ".container_$num_cols .pull_%-4d   { left:          %5dpx; }\n" );
 
 print <<'EOT';
-
-
 
 /* Clear Floated Elements
 ----------------------------------------------------------------------------------------------------*/
@@ -178,50 +139,51 @@ print <<'EOT';
 /* http://sonspring.com/journal/clearing-floats */
 
 .clear {
-	clear: both;
-	display: block;
-	overflow: hidden;
-	visibility: hidden;
-	width: 0;
-	height: 0;
+  clear: both;
+  display: block;
+  overflow: hidden;
+  visibility: hidden;
+  width: 0;
+  height: 0;
 }
 
 /* http://www.yuiblog.com/blog/2010/09/27/clearfix-reloaded-overflowhidden-demystified */
 
 .clearfix:before,
 .clearfix:after {
-	content: '\0020';
-	display: block;
-	overflow: hidden;
-	visibility: hidden;
-	width: 0;
-	height: 0;
+  content: '\0020';
+  display: block;
+  overflow: hidden;
+  visibility: hidden;
+  width: 0;
+  height: 0;
 }
 
-.clearfix:after {
-	clear: both;
-}
+.clearfix:after { clear: both; }
 
 /*
   The following zoom:1 rule is specifically for IE6 + IE7.
   Move to separate stylesheet if invalid CSS is a problem.
 */
 
-.clearfix {
-	zoom: 1;
-}
+.clearfix { zoom: 1; }
 EOT
 
 exit;
+
+sub generate_classes
+  {
+  my ( $max, $px_map, $format ) = @_;
+  printf $format, $_, $px_map->{$_}
+    for 1 .. $max;
+  }
 
 sub simple_spacer_header {
   my ( $text ) = @_;
   print <<"EOT";
 
-
 /* $text >> $num_cols Columns
 ----------------------------------------------------------------------------------------------------*/
-
 
 EOT
   return;
